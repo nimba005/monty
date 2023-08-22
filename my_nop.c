708d061b5f2b6a97ef5d0adc8120896188af7cdd
nimba005 <nimbaoyugi@gmail.com>
2023-08-22 18:03:32 +0000

    monty

---
+++ my_nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - Do great nothing
+ * @stack: pointer to stack
+ * @l_n: line number
+ */
+void nop(stack_t **stack, unsigned int l_n)
+{
+(void) stack;
+(void) l_n;
+}
